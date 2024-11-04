051814da03cd6b5d9d2c78c7499d23d2a667ad6e
Jack <jackmh01@gmail.com>
2024-11-04 03:16:12 +0000

    Created api.js

---
+++ src/api/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+// Create new axios instance with the base url for restcountries api
+const api = axios.create({
+    baseURL: 'https://restcountries.com/v3.1',
+});
+
+// Get request using instance above for all countries
+export const getAllCountries = async () => {
+    try {
+        const response = await api.get('/all');
+        return response.data;
+    }
+    catch (error) {
+        console.error ('Error could not get all countries', error);
+    }
+}
+
+// Get request using instance above for singular country by name parameter
+export const getCountryByName = async (name) => {
+    try {
+        const response = await api.get('/name/${name}');
+        return response.data[0];
+    }
+    catch (error) {
+        console.error('Error could not get country', error);
+    }
+}
